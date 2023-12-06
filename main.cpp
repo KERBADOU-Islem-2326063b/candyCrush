7345b2b0158d6f81af90ad78c3abb49350b07ffc
doop <lefundarki@yahoo.fr>
2023-12-06 15:19:50 +0100

    Ajouts de la sélection colorée, score, boucle du jeu complétée

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iomanip>
 #include <algorithm>
+
 using namespace std;
 
 //***********************************************************************************/
@@ -21,10 +22,7 @@ const unsigned KJaune   (33);
 const unsigned KBleu    (34);
 const unsigned KMagenta (35);
 const unsigned KCyan    (36);
-const unsigned KBGNoir    (40);
-const unsigned KBGRouge   (41);
-const unsigned KGBBleu    (44);
-const unsigned KBGCyan    (46);
+const unsigned KBlanc   (37);
 
 void couleur (const unsigned & coul) {
     cout << "\033[" << coul << "m";
@@ -110,24 +108,27 @@ void  afficheMatriceV1 (const CMatrice & Mat) {
 
 // affichage de la matrice avec les numéros de lignes / colonnes en haut / à gauche et avec un fond de couleur
 //pour signifier que la case est a KAIgnorer
-void  afficheMatriceV2 (const CMatrice & Mat) {
-    for (size_t x = 0; x < Mat.size(); ++x){
+void afficheMatriceV2 (const CMatrice & Mat, unsigned & score, unsigned rowSelect = 99, unsigned colSelect = 99) {
+    cout << " ";
+    for (size_t x = 0; x <= Mat.size(); ++x){
         cout << x << " ";
     }
+    cout << string(10, ' ') << "Score: " << score;
     cout << endl;
     for (size_t i = 0; i < Mat.size(); ++i){
-        cout << i + 1 << " ";
+        cout << setw(2) << i + 1 << " ";
         for(size_t j = 0; j < Mat[i].size(); ++j){
             switch(Mat[i][j]){
                 case 1: couleur(KReset); break;
-                case 2: couleur(KNoir); break;
+                case 2: couleur(KVert); break;
                 case 3: couleur(KRouge); break;
                 case 4: couleur(KJaune); break;
                 case 5: couleur(KBleu); break;
                 case 6: couleur(KMagenta); break;
-                case 7: couleur(KVert); break;
+                case 7: couleur(KNoir); break;
                 default: couleur(KCyan); break;
             }
+            if (i == rowSelect && j == colSelect && colSelect != 99 && rowSelect != 99) fond(KBlanc);
             if (Mat[i][j] == KAIgnorer) fond(KRouge);
             cout << Mat[i][j] << " ";
             couleur(KReset);
@@ -148,7 +149,7 @@ void explosionUneBombeHorizontale (CMatrice & mat, const size_t & numLigne,
                                     const size_t & numColonne, const size_t & combien);
 
 //
-bool detectionExplositionUneBombeHorizontale (CMatrice & mat){
+bool detectionExplositionUneBombeHorizontale (CMatrice & mat, unsigned & score){
     bool auMoinsUneExplosion (false);
 
     //on parcours la matrice case / case
@@ -162,9 +163,10 @@ bool detectionExplositionUneBombeHorizontale (CMatrice & mat){
             if (mat[i][j] == KAIgnorer) continue;
             combienALaSuite = 1;
 
-            for (size_t k (j+1); k < mat[i].size () && mat[i][j] == mat[i][k]; ++k) ++combienALaSuite;
+            for (size_t k (j+1); k < mat[i].size() && mat[i][j] == mat[i][k]; ++k) ++combienALaSuite;
             if (combienALaSuite >= 3){
                 auMoinsUneExplosion = true;
+                score += combienALaSuite;
                 /*cout << "on a une suite en position ligne = " << i+1
                      << "; colonne = " << j+1
                      << "; sur  " << combienALaSuite << " cases" << endl;
@@ -239,58 +241,65 @@ void faitUnMouvement (CMatrice & mat, const char & deplacement, const size_t & n
 }
 
 
-int ppalExo01 (){
-    CMatrice mat;
-    initMat(mat);
-    // affichage de la matrice sans les numéros de lignes / colonnes en haut / à gauche
-    afficheMatriceV0 (mat);
-    detectionExplositionUneBombeHorizontale (mat);
-    return 0;
-}
+bool detectionExplositionUneBombeVerticale (CMatrice & mat){
+    bool auMoinsUneExplosion (false);
 
-int ppalExo02 (){
-    CMatrice mat;
-    initMat(mat);
-    // affichage de la matrice sans les numéros de lignes / colonnes en haut / à gauche
-    afficheMatriceV1 (mat);
-    detectionExplositionUneBombeHorizontale (mat);
-    afficheMatriceV1 (mat);
-    return 0;
-}
+    //on parcours la matrice case / case
+    // si on tombe sur la valeur KAIgnorer, on passe a la case suivante
+    // sinon on compte combien de fois on a la même valeur
+    size_t combienALaSuite (1);
+    //si on a aun moins 3 chiffres identiques a la suite
 
-int ppalExo03 (){
-    CMatrice mat;
-    initMat(mat);
-    // affichage de la matrice sans les numéros de lignes / colonnes en haut / à gauche
-    afficheMatriceV1 (mat);
-    while (detectionExplositionUneBombeHorizontale (mat)) {
-        mat[5][8] = 4;
-        afficheMatriceV1 (mat);
+    for(size_t i = 0; i < mat.size(); ++i){
+        for(size_t j = 0; j < mat[i].size(); ++j){
+            if (mat[i][j] == KAIgnorer) continue;
+            combienALaSuite = 1;
+
+            for (size_t k (j+1); k < mat[i].size () && mat[i][j] == mat[k][j]; ++k) ++combienALaSuite;
+            if (combienALaSuite >= 3){
+                auMoinsUneExplosion = true;
+                /*cout << "on a une suite en position ligne = " << i+1
+                     << "; colonne = " << j+1
+                     << "; sur  " << combienALaSuite << " cases" << endl;
+                cout << string (20, '-') << endl << "matrice avant suppression" << endl;
+                afficheMatriceV2(mat);*/
+                //explosionUneBombeVerticale (mat, i, j, combienALaSuite);
+                //cout << string (20, '-') << endl << "matrice après suppression" << endl;
+                //afficheMatriceV2(mat);
+
+            }
+        }
     }
-    return 0;
+
+    return auMoinsUneExplosion;
 }
+
 
 int ppalExo04 (){
     CMatrice mat;
+    unsigned score (0);
     initMat(mat);
     // affichage de la matrice sans les numéros de lignes / colonnes en haut / à gauche
-    detectionExplositionUneBombeHorizontale (mat);
-    afficheMatriceV2 (mat);
+    detectionExplositionUneBombeHorizontale (mat, score);
+    afficheMatriceV2 (mat, score);
     //condition de victoire a trouver
     while (true) {
         cout << "Fait un mouvement ";
         cout << "numero de ligne : ";
         size_t numLigne;
+        cin >> numLigne;
         cout << numLigne;
         cout << "numero de colonne : ";
         size_t numCol;
+        cin >> numCol;
         cout << numCol;
+        afficheMatriceV2(mat, score, numLigne-1, numCol-1);
         cout << "Sens du deplacement : (A|Z|E|Q|D|W|X|C) : " << endl;
         char deplacement;
         cin >> deplacement;
-        faitUnMouvement (mat, deplacement, numLigne, numCol);
-        detectionExplositionUneBombeHorizontale (mat);
-        afficheMatriceV2 (mat);
+        faitUnMouvement (mat, deplacement, numLigne-1, numCol-1);
+        detectionExplositionUneBombeHorizontale (mat, score);
+        afficheMatriceV2 (mat, score);
     }
     return 0;
 }
@@ -313,10 +322,10 @@ int main() {
     //-------------------------------------//
 
     // ---------Exercice 3 -----------------//
-    return ppalExo03();
+    //return ppalExo03();
     //-------------------------------------//
 
     // ---------Exercice 3 -----------------//
-    //return ppalExo04();
+    return ppalExo04();
     //-------------------------------------//
 }
