528d3de51a48889e9f5e16ba1422c2451dae4505
ODERZO-Flavio-2326083b <flavio.oderzo@etu.univ-amu.fr>
2023-12-17 21:08:31 +0100

    Ajout d'un éditeur de niveaux

--- main.cpp
+++ main.cpp
@@ -36,78 +36,7 @@ typedef vector <CVLigne> CMatrice; // un type représentant la grille
 const contenueDUneCase KAIgnorer = 0;
 const contenueDUneCase KPlusGrandNombreDansLaMatrice = 4;
 
-void importNv(CMatrice & mat, const string & nomNv){
-    ifstream fichNv;
-    fichNv.open(nomNv);
 
-    unsigned nb, haut, larg;
-
-    fichNv >> haut;
-    fichNv >> larg;
-
-    mat.resize(haut);
-    for(unsigned i = 0; i < haut; ++i){
-        mat[i].resize(larg);
-    }
-
-    unsigned comptHaut=0, comptLarg=0;
-    while(fichNv >> nb){
-        cout << nb << endl;
-        if(comptLarg >= larg){
-            comptLarg = 0;
-            ++comptHaut;
-        }
-
-        mat[comptLarg][comptHaut] = nb;
-
-        ++comptLarg;
-    }
-
-    fichNv.close();
-}
-
-void editNv (CMatrice & mat);
-
-//initialisation de la grille de jeu
-void initMat (CMatrice & mat, const size_t & nbLignes = 10,
-             const size_t & nbColonnes = 10,
-              const unsigned & nbMax= KPlusGrandNombreDansLaMatrice){
-    char mode;
-
-    while (1) {
-        cout << "Choisir un mode :" << endl
-             << "Importer un niveau (i)" << endl
-             << "Niveau aléatoire (n)" << endl
-             << "Éditeur de niveaux (e)" << endl;
-        cin >> mode;
-
-        mode = tolower(mode);
-        if (mode == 'i'){
-            string lvlName;
-            cout << "Entrer le nom du fichier niveau :" << endl;
-            cin >> lvlName;
-            importNv(mat, lvlName);
-            break;
-
-        } else if (mode == 'n'){
-            mat.resize(nbLignes);
-            for (size_t i = 0; i < nbLignes; ++i){
-                mat[i].resize(nbColonnes);
-                for(size_t j = 0; j < nbColonnes; ++j){
-                    mat[i][j] = rand() % nbMax + 1;
-                }
-            }
-            break;
-
-        } else if (mode == 'e'){
-            cout << "Entrée dans l'éditeur de niveaux..." << endl;
-            // editNv(mat);
-        } else {
-            cout << "Mauvais choix" << endl;
-            continue;
-        }
-    }
-}
 
 // affichage de la matrice sans les numéros de lignes / colonnes en haut / à gauche
 void  afficheMatriceV0 (const CMatrice & Mat) {
@@ -155,13 +84,14 @@ void  afficheMatriceV1 (const CMatrice & Mat) {
 
 // affichage de la matrice avec les numéros de lignes / colonnes en haut / à gauche et avec un fond de couleur
 //pour signifier que la case est a KAIgnorer
-void afficheMatriceV2 (const CMatrice & Mat, unsigned & score, unsigned rowSelect = 99, unsigned colSelect = 99) {
+void afficheMatriceV2 (const CMatrice & Mat, const unsigned & score = 0,
+                      const unsigned & rowSelect = 99, const unsigned & colSelect = 99) {
     cout << "   | ";
     for (size_t x = 0; x <= Mat.size(); ++x){
         if (x == 0) continue;
         cout << x << " | ";
     }
-    cout << string(10, ' ') << "Score: " << score;
+    if (score > 0) cout << string(10, ' ') << "Score: " << score;
     cout << endl;
     for (size_t i = 0; i < Mat.size(); ++i){
         cout << setw(2) << i + 1 << " | ";
@@ -235,8 +165,8 @@ bool detectionExplosionUneBombeHorizontale (CMatrice & mat, unsigned & score){
 }
 
 
-void faitUnMouvement (CMatrice & mat, const char & deplacement, const size_t & numLigne,
-                     const size_t & numCol) {
+void faitUnMouvement (CMatrice & mat, const char & deplacement,
+                     const size_t & numLigne, const size_t & numCol) {
 
     size_t nvPosLigne (numLigne), nvPosCol (numCol);
     switch (tolower(deplacement)) {
@@ -314,6 +244,134 @@ bool detectionExplosionUneBombeVerticale (CMatrice & mat, unsigned & score){
     return auMoinsUneExplosion;
 }
 
+void importNv(CMatrice & mat, const string & nomNv){
+    ifstream fichNv;
+    fichNv.open(nomNv);
+
+    unsigned nb, haut, larg,
+        comptHaut=0, comptLarg=0;
+    fichNv >> haut;
+    fichNv >> larg;
+
+    mat.resize(haut);
+    for(unsigned i = 0; i < haut; ++i){
+        mat[i].resize(larg);
+    }
+
+    while(fichNv >> nb){
+        if(comptLarg >= larg){
+            comptLarg = 0;
+            ++comptHaut;
+        }
+
+        mat[comptLarg][comptHaut] = nb;
+
+        ++comptLarg;
+    }
+
+    fichNv.close();
+}
+
+void saveNv (CMatrice & mat, const string & nomNv){
+    ofstream fichNv;
+    unsigned haut, larg;
+    fichNv.open(nomNv + ".txt");
+
+    haut = mat.size();
+    larg = mat[0].size();
+
+    fichNv << haut << " " << larg << endl;
+
+    for(size_t i = 0; i < mat.size(); ++i){
+        for(size_t j = 0; j < mat[i].size(); ++j){
+            fichNv << mat[i][j] << " ";
+        }
+        fichNv << endl;
+    }
+
+    fichNv.close();
+}
+
+void editNv (CMatrice & mat){
+    string nomNv;
+
+    unsigned haut, larg, nb;
+
+    cout << "Bienvenue dans l'éditeur de niveaux" << endl
+         << "Nommez votre magnifique niveau :" << endl;
+    cin >> nomNv;
+    cout << "Nombre de colonnes ?" << endl;
+    cin >> larg;
+    cout << "Nombre de lignes ?" << endl;
+    cin >> haut;
+
+    mat.resize(larg);
+    for (size_t i = 0; i < larg; ++i){
+        mat[i].resize(haut);
+    }
+
+    for (size_t i = 0; i < larg; ++i){
+        for(size_t j = 0; j < haut; ++j){
+            afficheMatriceV2(mat, 0 , i, j);
+            cout << endl;
+            cout << "Quel nombre placer dans la case surlignée ?" << endl;
+            cin >> nb;
+            mat[i][j] = nb;
+        }
+    }
+
+    afficheMatriceV2(mat);
+    char choix;
+    cout << endl << "Voici votre magnifique niveau, voulez-vous le sauvegarder? (y/n)" << endl;
+    cin >> choix;
+
+    if (choix == 'y') saveNv(mat, nomNv);
+
+
+};
+
+//initialisation de la grille de jeu
+void initMat (CMatrice & mat, const size_t & nbLignes = 10,
+             const size_t & nbColonnes = 10,
+             const unsigned & nbMax= KPlusGrandNombreDansLaMatrice){
+    char mode;
+
+    while (1) {
+        cout << "Choisir un mode :" << endl
+             << "Importer un niveau (i)" << endl
+             << "Niveau aléatoire (n)" << endl
+             << "Éditeur de niveaux (e)" << endl;
+                    cin >> mode;
+
+        mode = tolower(mode);
+        if (mode == 'i'){
+            string lvlName;
+            cout << "Entrer le nom du fichier niveau :" << endl;
+            cin >> lvlName;
+            importNv(mat, lvlName);
+            break;
+
+        } else if (mode == 'n'){
+            mat.resize(nbLignes);
+            for (size_t i = 0; i < nbLignes; ++i){
+                mat[i].resize(nbColonnes);
+                for(size_t j = 0; j < nbColonnes; ++j){
+                    mat[i][j] = rand() % nbMax + 1;
+                }
+            }
+            break;
+
+        } else if (mode == 'e'){
+            cout << "Entrée dans l'éditeur de niveaux..." << endl;
+            editNv(mat);
+            break;
+        } else {
+            cout << "Mauvais choix" << endl;
+            continue;
+        }
+    }
+}
+
 int game (){
     CMatrice mat;
     unsigned score (0);
@@ -339,8 +397,9 @@ int game (){
         cin >> deplacement;
         if (deplacement == 'f') continue;
         faitUnMouvement (mat, deplacement, numLigne-1, numCol-1);
-        detectionExplosionUneBombeHorizontale (mat, score);
-        detectionExplosionUneBombeVerticale(mat, score);
+
+        while(detectionExplosionUneBombeHorizontale (mat, score));
+        while(detectionExplosionUneBombeVerticale(mat, score));
         afficheMatriceV2 (mat, score);
     }
     return 0;
